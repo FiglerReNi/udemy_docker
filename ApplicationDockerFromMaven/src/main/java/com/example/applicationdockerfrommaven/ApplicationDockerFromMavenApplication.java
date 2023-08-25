be361221b5bae3fc2bbe064a247815db4b790c7a
figlerr <figler.renata@tmx.hu>
2023-08-25 16:00:51 +0200

    forras

---
+++ ApplicationDockerFromMaven/src/main/java/com/example/applicationdockerfrommaven/ApplicationDockerFromMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.applicationdockerfrommaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationDockerFromMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationDockerFromMavenApplication.class, args);
+    }
+
+}
---
+++ ApplicationDockerFromMaven/src/test/java/com/example/applicationdockerfrommaven/ApplicationDockerFromMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationdockerfrommaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDockerFromMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
